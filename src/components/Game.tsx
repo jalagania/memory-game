eb27c687fae7013626a142bde9d867b959ed2c6d
jalagania <beka>
2023-04-19 21:07:13 +0400

    added game board

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,13 +1,27 @@
+import * as _ from "lodash";
+import { useGlobalContext } from "../context";
 import Footer from "./Footer";
 import "./Game.css";
 import Header from "./Header";
 
 function Game() {
+  const { gridSize } = useGlobalContext();
+  let items = Array.from(Array((gridSize * gridSize) / 2).keys());
+  items = _.shuffle(items.concat(items));
+
   return (
     <div className="game-container">
-      <Header />
-      <main className="game-board"></main>
-      <Footer />
+      <div className="game-wrapper">
+        <Header />
+        <main className={`game-board ${gridSize === 4 ? "grid-4" : "grid-6"}`}>
+          {items.map((item, index) => (
+            <button key={index} className="btn-item">
+              {item}
+            </button>
+          ))}
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }

a5f088dbe4620c86dedfd885de09a9be1741cf05
jalagania <beka>
2023-04-20 12:09:19 +0400

    added icons

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,23 +1,29 @@
 import * as _ from "lodash";
 import { useGlobalContext } from "../context";
+import { iconpack } from "../data";
 import Footer from "./Footer";
 import "./Game.css";
 import Header from "./Header";
 
 function Game() {
   const { theme, gridSize } = useGlobalContext();
-  let items = Array.from(Array((gridSize * gridSize) / 2).keys());
-  items = _.shuffle(items.concat(items));
+  let numbers = Array.from(Array((gridSize * gridSize) / 2).keys());
+  numbers = _.shuffle(numbers.concat(numbers));
+  let icons = iconpack.slice(0, (gridSize * gridSize) / 2);
+  icons = _.shuffle(icons.concat(icons));
+  const boardItems = theme === "numbers" ? numbers : icons;
 
   return (
     <div className="game-container">
       <div className="game-wrapper">
         <Header />
         <main className={`game-board ${gridSize === 4 ? "grid-4" : "grid-6"}`}>
-          {items.map((item, index) => (
+          {boardItems.map((item, index) => (
             <button key={index} className="btn-item">
               {theme === "numbers" && <p>{item}</p>}
-              {theme === "icons" && <p>{item}</p>}
+              {theme === "icons" && (
+                <img src={`./icons/${item}.svg`} alt={item.toString()} />
+              )}
             </button>
           ))}
         </main>
